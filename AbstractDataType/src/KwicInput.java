a5d8eec3f6ae377daf98374d76a104d72563c993
turtle96 <angjazlyn@hotmail.com>
2017-01-23 22:21:19 +0800

    input implementation

---
+++ AbstractDataType/src/KwicInput.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class KwicInput implements Input {
+    
+    private ArrayList<ArrayList<String>> titles;
+    private ArrayList<String> wordsToIgnore;
+    
+    private static final Logger LOGGER = Logger.getLogger(KwicInput.class.getName());
+
+    @Override
+    public void getInput() {
+        Scanner scanner = new Scanner(System.in);
+        
+        //TODO exception handling
+        System.out.println("Enter words to ignore (in a single line with each word separated by a space):");
+        wordsToIgnore = new ArrayList<>(Arrays.asList(scanner.nextLine().split(" ")));
+        
+        LOGGER.info("Words to ignore: " + wordsToIgnore.toString());
+        
+        System.out.println("Enter titles to index (press enter after each title, and type 'end' to finish): ");
+        ArrayList<String> titles = new ArrayList<>();
+        
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            titles.add(input);
+        }
+        
+        LOGGER.info("Titles: " + titles.toString());
+        
+        scanner.close();
+    }
+
+    @Override
+    public ArrayList<ArrayList<String>> getTitles() {
+        return titles;
+    }
+
+    @Override
+    public ArrayList<String> getWordsToIgnore() {
+        return wordsToIgnore;
+    }
+
+}
---
+++ AbstractDataType/src/MainControl.java
@@ -0,0 +1,13 @@
+/** Main class
+* */
+public class MainControl {
+
+    public static void main(String[] args) {
+        Input input = new KwicInput();
+        
+        input.getInput();
+        
+        
+    }
+
+}
