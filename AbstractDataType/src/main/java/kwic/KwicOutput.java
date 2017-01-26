d297b91b5edbb23b28c4d6a0b198bb2fcde09ad2
turtle96 <angjazlyn@hotmail.com>
2017-01-27 01:28:53 +0800

    add write output to file

--- AbstractDataType/src/main/java/kwic/KwicOutput.java
+++ AbstractDataType/src/main/java/kwic/KwicOutput.java
@@ -1,15 +1,41 @@
 package kwic;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.logging.Logger;
 
 /** Implements Output interface to display indexed list
  * */
 public class KwicOutput implements Output {
 
-	@Override
+    private static final Logger LOGGER = Logger.getLogger(KwicOutput.class.getName());
+    
+	private static final String FILEPATH = "src\\output\\";
+
+    @Override
 	public void display(ArrayList<ArrayList<String>> titles, ArrayList<Address> addresses) {
 	    TextUI.printIndexOutputMessage();
 	    
-		for (Address address: addresses) {
+		String index = printIndex(titles, addresses);
+		
+		writeToFile(index);
+	}
+
+    private void writeToFile(String index) {
+        
+        try {
+            Files.write(Paths.get(FILEPATH + "output.txt"), index.getBytes());
+        } catch (IOException e) {
+            LOGGER.warning("File write error");
+        }
+    }
+
+    private String printIndex(ArrayList<ArrayList<String>> titles, ArrayList<Address> addresses) {
+        StringBuilder index = new StringBuilder();
+        
+        for (Address address: addresses) {
 			ArrayList<String> title = titles.get(address.getLineIndex());
 			int titleLength = title.size();
 			int startIndex = address.getStartIndex();
@@ -26,7 +52,12 @@ public class KwicOutput implements Output {
 			}
 			
 			System.out.println(displayTitle.trim());
+			
+			index.append(displayTitle.trim() + "\r\n");  //for some reason \n cannot be written to file
+			                                             //so \r\n is used instead
         }
-	}
+
+        return index.toString();
+    }
 
 }
