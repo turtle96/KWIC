45ce5bf0f100c055dcbc75e29f2d3cdfb6a7ae85
turtle96 <angjazlyn@hotmail.com>
2017-01-27 00:54:59 +0800

    refactor

--- AbstractDataType/src/main/java/kwic/MainControl.java
+++ AbstractDataType/src/main/java/kwic/MainControl.java
@@ -10,20 +10,40 @@ public class MainControl {
 
         TextUI.printWelcomeMessage();
         
-        Input input = new KwicInput();
-        input.readInput(new Scanner(System.in));
+        Input input = executeInput();
         ArrayList<ArrayList<String>> titles = input.getTitles();
 
-        CircularShift circularShift = new KwicCircularShift();
-        circularShift.shift(titles, input.getWordsToIgnore());
-        ArrayList<Address> shiftedAddresses = circularShift.getAddresses();
+        ArrayList<Address> shiftedAddresses = executeCircularShift(input, titles);
 
+        ArrayList<Address> sortedAddresses = executeAlphabetize(titles, shiftedAddresses);
+
+        executeOutput(titles, sortedAddresses);
+    }
+
+    private static void executeOutput(ArrayList<ArrayList<String>> titles, ArrayList<Address> sortedAddresses) {
+        Output output = new KwicOutput();
+        output.display(titles, sortedAddresses);
+    }
+
+    private static ArrayList<Address> executeAlphabetize(ArrayList<ArrayList<String>> titles,
+            ArrayList<Address> shiftedAddresses) {
         Alphabetize alphabetize = new KwicAlphabetize(shiftedAddresses);
         alphabetize.sort(titles);
         ArrayList<Address> sortedAddresses = alphabetize.getAddresses();
+        return sortedAddresses;
+    }
 
-        Output output = new KwicOutput();
-        output.display(titles, sortedAddresses);
+    private static ArrayList<Address> executeCircularShift(Input input, ArrayList<ArrayList<String>> titles) {
+        CircularShift circularShift = new KwicCircularShift();
+        circularShift.shift(titles, input.getWordsToIgnore());
+        ArrayList<Address> shiftedAddresses = circularShift.getAddresses();
+        return shiftedAddresses;
+    }
+
+    private static Input executeInput() {
+        Input input = new KwicInput();
+        input.readInput(new Scanner(System.in));
+        return input;
     }
 
 }
